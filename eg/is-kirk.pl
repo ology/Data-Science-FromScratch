4657fc934134cb629d3b2edae447e72bc93008da
Gene Boggs <gene@ology.net>
2020-04-30 21:46:50 -0700

    Illustration of NaiveBayes analysis

---
+++ eg/is-kirk.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# Did Kirk say it? - Naive Bayes analysis of Kirk, Spock and McCoy lines.
+# spam = is kirk. ham = is not kirk
+# Star Trek scripts used:
+# https://github.com/ology/Machine-Learning/blob/master/Kirk-Spock-McCoy.zip
+
+use Data::Science::FromScratch;
+use File::Slurper qw(read_text);
+use Lingua::EN::Sentence qw(get_sentences);
+
+my $path = $ENV{HOME} . '/Documents/lit/Kirk-Spock-McCoy/';
+
+my @messages;
+
+print "Gathering messages...\n";
+
+for my $name (qw(kirk spock mccoy)) {
+    my $file = $path . $name . '.txt';
+
+    my $content = read_text($file);
+
+    my $sentences = get_sentences($content);
+
+    for my $sentence (@$sentences) {
+        $sentence =~ s/\(.+?\)//; # Remove action instructions
+
+        next if $sentence =~ /\(/; # Skip broken actions
+
+        $sentence =~ s/\n+/ /g; # Make the sentence a single line
+        $sentence =~ s/^\s*//; # Trim whitespace
+        $sentence =~ s/\s*$//; # Trim whitespace
+
+        next unless $sentence =~ /\w/;
+
+    #    print $sentence, "\n\n";
+
+        push @messages, { text => $sentence, is_spam => $name eq 'kirk' ? 1 : 0 };
+    }
+}
+
+my $ds = Data::Science::FromScratch->new;
+
+print "Training on messages...\n";
+
+$ds->train(@messages);
+
+print 'Kirk said ', $ds->spam_messages, " words.\n";
+print 'Spock or McCoy said ', $ds->ham_messages, " words.\n";
+
+print "Probability that Kirk said,\n";
+for my $text (
+    'Shall we pick some flowers, Doctor?',                      # Kirk
+    'Vulcan has no moon, Miss Uhura.',                          # Spock
+    'Is that how you get girls to like you, by bribing them?',  # Mccoy
+    'Give me warp nine, Scotty.',                               # Fake Kirk
+    'That is highly illogical.',                                # Fake Spock
+    "He's dead, Jim.",                                          # Fake McCoy
+) {
+    my $prediction = $ds->nb_predict($text);
+    printf qq/\t"%s" = %.4f\n/, $text, $prediction;
+}
