95e61eaaabdb310c6334710cd06d734688e0760c
Gene Boggs <gene@ology.net>
2020-05-02 13:43:18 -0700

    Add Linear

--- lib/Data/Science/FromScratch/Layer.pm
+++ lib/Data/Science/FromScratch/Layer.pm
@@ -1,5 +1,6 @@
 package Data::Science::FromScratch::Layer;
 
+use Data::Science::FromScratch;
 use Moo;
 use strictures 2;
 use namespace::clean;
@@ -34,7 +35,7 @@ has ds => (
 
 =head2 forward
 
-  my $x = $layer->forward($input);
+  $v = $layer->forward($input);
 
 =cut
 
@@ -44,7 +45,7 @@ sub forward {
 
 =head2 backward
 
-  my $x = $layer->backward($gradient);
+  $v = $layer->backward($gradient);
 
 =cut
 
@@ -54,7 +55,7 @@ sub backward {
 
 =head2 params
 
-  my $x = $layer->params;
+  $v = $layer->params;
 
 =cut
 
@@ -65,7 +66,7 @@ sub params {
 
 =head2 grads
 
-  my $x = $layer->grads;
+  $v = $layer->grads;
 
 =cut
 
@@ -76,7 +77,6 @@ sub grads {
 
 package Data::Science::FromScratch::Sigmoid;
 
-use Data::Science::FromScratch;
 use Moo;
 use strictures 2;
 use namespace::clean;
@@ -133,6 +133,182 @@ sub backward {
         $self->sigmoids,
         $gradient
     );
+}
+
+package Data::Science::FromScratch::Linear;
+
+use Moo;
+use strictures 2;
+use namespace::clean;
+
+extends 'Data::Science::FromScratch::Layer';
+
+=head1 SYNOPSIS
+
+  use Data::Science::FromScratch::Linear;
+
+  my $linear = Data::Science::FromScratch::Linear->new;
+
+=head1 DESCRIPTION
+
+A C<Data::Science::FromScratch::Linear> is a class for building neural networks.
+
+=head1 ATTRIBUTES
+
+=head2 input_dim
+
+  $v = $linear->input_dim;
+
+=cut
+
+has input_dim => (
+    is => 'ro',
+);
+
+=head2 output_dim
+
+  $v = $linear->output_dim;
+
+=cut
+
+has output_dim => (
+    is => 'ro',
+);
+
+=head2 init
+
+  $x = $linear->init;
+
+Default: C<xavier>
+
+=cut
+
+has init => (
+    is      => 'ro',
+    default => sub { 'xavier' },
+);
+
+=head2 input
+
+  $v = $linear->input;
+
+=cut
+
+has input => (
+    is => 'rw',
+);
+
+=head2 w
+
+  $v = $linear->w;
+
+Weights
+
+=cut
+
+has w => (
+    is      => 'ro',
+    builder => 1,
+);
+
+sub _build_w {
+    my ($self) = @_;
+    return $self->ds->random_tensor([ $self->output_dim, $self->input_dim ], $self->init);
+}
+
+=head2 w_grad
+
+  $v = $linear->w_grad;
+
+=cut
+
+has w_grad => (
+    is => 'rw',
+);
+
+=head2 b
+
+  $v = $linear->b;
+
+Bias
+
+=cut
+
+has b => (
+    is      => 'ro',
+    builder => 1,
+);
+
+sub _build_b {
+    my ($self) = @_;
+    return $self->ds->random_tensor([ $self->output_dim ], $self->init);
+}
+
+=head2 b_grad
+
+  $v = $linear->b_grad;
+
+=cut
+
+has b_grad => (
+    is => 'rw',
+);
+
+=head1 METHODS
+
+=head2 forward
+
+  $v = $linear->forward($input);
+
+=cut
+
+sub forward {
+    my ($self, $input) = @_;
+    $self->input($input);
+    return [map { $self->ds->vector_dot($input, $self->w->[$_]) } 0 .. $self->output_dim];
+}
+
+=head2 backward
+
+  $v = $linear->backward($gradient);
+
+=cut
+
+sub backward {
+    my ($self, $gradient) = @_;
+    $self->b_grad($gradient);
+    my @w_grad;
+    for my $i (0 .. $self->output_dim) {
+        push @w_grad, [map { $self->input->[$_] * $gradient->[$i] } 0 .. $self->input_dim];
+    }
+    $self->w_grad(\@w_grad);
+    my @sum;
+    for my $i (0 .. $self->input_dim) {
+        push @sum, [map { $self->w->[$_][$i] * $gradient->[$_] } 0 .. $self->output_dim];
+    }
+    return \@sum;
+}
+
+=head2 params
+
+  $v = $linear->params;
+
+=cut
+
+sub params {
+    my ($self) = @_;
+    return [$self->w, $self->b];
+}
+
+=head2 grads
+
+  $v = $linear->grads;
+
+=cut
+
+sub grads {
+    my ($self) = @_;
+    return [$self->w_grad, $self->b_grad];
 }
 
 1;
