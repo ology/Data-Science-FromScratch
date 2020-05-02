74e22f03075738e8ac53123cda061d2c725ce1df
Gene Boggs <gene@ology.net>
2020-05-01 19:40:52 -0700

    Fix typo

--- eg/is-kirk.pl
+++ eg/is-kirk.pl
@@ -13,9 +13,9 @@ use Lingua::EN::Sentence qw(get_sentences);
 
 my $path = $ENV{HOME} . '/Documents/lit/Kirk-Spock-McCoy/';
 
-my @messages;
-
 print "Gathering messages...\n";
+
+my @messages;
 
 for my $name (qw(kirk spock mccoy)) {
     my $file = $path . $name . '.txt';
@@ -54,7 +54,7 @@ print "Probability that Kirk said,\n";
 for my $text (
     'Shall we pick some flowers, Doctor?',                      # Kirk
     'Vulcan has no moon, Miss Uhura.',                          # Spock
-    'Is that how you get girls to like you, by bribing them?',  # Mccoy
+    'Is that how you get girls to like you, by bribing them?',  # McCoy
     'Give me warp nine, Scotty.',                               # Fake Kirk
     'That is highly illogical.',                                # Fake Spock
     "He's dead, Jim.",                                          # Fake McCoy
