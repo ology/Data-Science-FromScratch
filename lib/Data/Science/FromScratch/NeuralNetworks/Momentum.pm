a569f02b6cdf44d34ea5a54be37a7cc55112bd39
Gene Boggs <gene@ology.net>
2020-05-02 19:56:19 -0700

    Add Momentum

---
+++ lib/Data/Science/FromScratch/NeuralNetworks/Momentum.pm
@@ -0,0 +1,91 @@
+package Data::Science::FromScratch::NeuralNetworks::Momentum;
+
+use Moo;
+use strictures 2;
+use namespace::clean;
+
+extends 'Data::Science::FromScratch::NeuralNetworks::Optimizer';
+
+=head1 SYNOPSIS
+
+  use Data::Science::FromScratch::NeuralNetworks::Momentum;
+
+  my $mo = Data::Science::FromScratch::NeuralNetworks::Momentum->new;
+
+=head1 DESCRIPTION
+
+A C<Data::Science::FromScratch::NeuralNetworks::Momentum> is an class for updating neural networks.
+
+=head1 ATTRIBUTES
+
+=head2 lr
+
+  $rate = $mo->lr;
+
+Learning rate
+
+=cut
+
+has lr => (
+    is => 'ro',
+);
+
+=head2 mo
+
+  $momentum = $mo->mo;
+
+Momentum
+
+=cut
+
+has mo => (
+    is => 'ro',
+);
+
+=head2 updates
+
+  $avg = $mo->updates;
+
+Running average
+
+=cut
+
+has updates => (
+    is => 'ro',
+);
+
+=head1 METHODS
+
+=head2 step
+
+  $mo->step($layer);
+
+=cut
+
+sub step {
+    my ($self, $layer) = @_;
+    unless ($self->updates) {
+        my $updates = [map { $self->ds->zeros_like($_) } @{ $layer->grads }];
+        $self->updates($updates);
+    }
+    for my $i (0 .. @{ $self->updates } - 1) {
+        $self->updates->[$i] = $self->ds->tensor_combine(
+            sub { my ($x, $y) = @_; return $self->mo * $x + (1 - $self->mo) * $y },
+            $self->updates->[$i],
+            $layer->grads->[$i]
+        );
+        $layer->params->[$i] = $self->ds->tensor_combine(
+            sub { my ($x, $y) = @_; return $x - $self->lr * $y },
+            $layer->params->[$i],
+            $self->updates->[$i]
+        );
+    }
+}
+
+1;
+
+=head1 SEE ALSO
+
+L<Data::Science::FromScratch::NeuralNetworks::Optimizer>
+
+=cut
