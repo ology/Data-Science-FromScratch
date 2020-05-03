6537e19f1ff1d69570431f7b27fc93b4aebd2a38
Gene Boggs <gene@ology.net>
2020-05-02 18:23:00 -0700

    Add NNOptimizer

---
+++ lib/Data/Science/FromScratch/NNOptimizer.pm
@@ -0,0 +1,53 @@
+package Data::Science::FromScratch::NNOptimizer;
+
+use Data::Science::FromScratch;
+use Moo;
+use strictures 2;
+use namespace::clean;
+
+=head1 SYNOPSIS
+
+  # This module is a base class and should not be used directly.
+
+=head1 DESCRIPTION
+
+A C<Data::Science::FromScratch::NNOptimizer> is a class for updating neural networks.
+
+=head1 ATTRIBUTES
+
+=head2 ds
+
+  $obj = $opt->ds;
+
+C<Data::Science::FromScratch> object
+
+=cut
+
+has ds => (
+    is        => 'ro',
+    lazy      => 1,
+    init_args => undef,
+    default   => sub { Data::Science::FromScratch->new },
+);
+
+=head1 METHODS
+
+=head2 step
+
+  $opt->step($layer);
+
+=cut
+
+sub step {
+    my ($self, $layer) = @_;
+}
+
+1;
+
+=head1 SEE ALSO
+
+F<t/015-deep-learning.t>
+
+L<Data::Science::FromScratch::NNGradientDescent>
+
+=cut
