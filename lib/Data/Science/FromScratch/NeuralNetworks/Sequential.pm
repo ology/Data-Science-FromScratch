f65cf70f8fb51742ddb3a2e532f37af499933862
Gene Boggs <gene@ology.net>
2020-05-03 01:15:19 -0700

    Remove code not in the book

--- lib/Data/Science/FromScratch/NeuralNetworks/Sequential.pm
+++ lib/Data/Science/FromScratch/NeuralNetworks/Sequential.pm
@@ -70,7 +70,6 @@ sub params {
     my ($self) = @_;
     my @params;
     for my $layer (@{ $self->layers }) {
-        next unless $layer->params;
         push @params, @{ $layer->params };
     }
     return \@params;
@@ -86,7 +85,6 @@ sub grads {
     my ($self) = @_;
     my @grads;
     for my $layer (reverse @{ $self->layers }) {
-        next unless $layer->grads;
         push @grads, @{ $layer->grads };
     }
     return \@grads;
